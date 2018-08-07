d0a869e1ffb2f8f67a1b76864e671647d0a7f9a5
paul@paulleach.co.uk <paul@paulleach.co.uk>
2018-08-07 09:16:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function increaseRankBy(n) {
   
   for(var i = 0; i < lis.length; i++){
     currentAmount = parseInt(lis[i].innerText);
-    currentAmount = currentAmount + n;
+    currentAmount = currentAmount + 3;
     lis[i].innerText = currentAmount;
   }
   return lis;
