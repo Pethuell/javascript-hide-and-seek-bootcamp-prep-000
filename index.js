e2a77729d64ba3d44060c8effccf9c0becc48037
paul@paulleach.co.uk <paul@paulleach.co.uk>
2018-08-07 09:39:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  var lis = document.querySelectorAll('.ranked-list li');
+  const lis = document.querySelectorAll('ul.ranked-list li');
   var currentAmount;
   
   for(var i = 0; i < lis.length; i++){
