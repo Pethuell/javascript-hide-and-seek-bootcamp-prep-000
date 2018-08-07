9e41b5e63c7b6021ed0a1cef5112a92220bc122d
paul@paulleach.co.uk <paul@paulleach.co.uk>
2018-08-07 09:44:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+mocha.run();
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li');
+  
+  for(var i = 0; i < lis.length; i++){
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
+  }
+}
+
+function deepestChild() {
+  var deepest = document.querySelector('div#grand-node');
+  do {
+    deepest = deepest.querySelector('*');
+  } while (deepest.querySelector('*') !== null)
+  
+  return deepest;
+}
