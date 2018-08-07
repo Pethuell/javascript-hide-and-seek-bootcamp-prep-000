7dda010f18330072708220c9e3d31b8a6a464eeb
paul@paulleach.co.uk <paul@paulleach.co.uk>
2018-08-07 09:12:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+mocha.run();
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var lis = document.querySelectorAll('.ranked-list li');
+  var currentAmount;
+  
+  for(var i = 0; i < lis.length; i++){
+    currentAmount = parseInt(lis[i].innerText);
+    currentAmount = currentAmount + n;
+    lis[i].innerText = currentAmount;
+  }
+}
+
+function deepestChild() {
+  
+}
